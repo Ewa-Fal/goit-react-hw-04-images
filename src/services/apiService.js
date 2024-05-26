79f4cc669ee09c17fd1eae88464ff3368322c368
Ewa <efaltynska@gmail.com>
2024-05-26 14:15:32 +0200

    commit

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+/*import axios from 'axios';
 
 const api = async (query, page) => {
   const searchParams = new URLSearchParams({
@@ -19,3 +19,4 @@ const api = async (query, page) => {
 };
 
 export default api;
+*/
